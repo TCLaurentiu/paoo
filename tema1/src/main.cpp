160c95c7a0736a1f30d6dcfd4418c64d0015f397
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2023-10-28 16:13:35 +0300

    Use the methods of the String in main

--- tema1/src/main.cpp
+++ tema1/src/main.cpp
@@ -4,5 +4,14 @@
 using std::cout;
 
 int main(){
+  char name[] = "Claudiu", name2[] = "Andrei", name3[] = "Miruna";
+  String s_name = String(name);
+  String s_name2 = String(s_name); 
+  String s_name3 = String(name2);
+  
+  String s_name4 = std::move(s_name3);
+
+  s_name4.append(name3);
+  s_name4.append(s_name);
 
 }
