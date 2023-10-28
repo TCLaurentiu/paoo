fb6adf8d0f15244b79d390546f56a9bcc9f8815c
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2023-10-28 16:07:21 +0300

    Implement the constructor for the String class

--- tema1/src/string/string.cpp
+++ tema1/src/string/string.cpp
@@ -1,1 +1,15 @@
 #include "string.hpp"
+#include <string.h>
+#include <iostream>
+
+using std::cout;
+
+String::String(char *c_string){
+
+  cout << c_string << " constructor called \n";
+
+  this->size = strlen(c_string);
+  this->capacity = this->size;
+  this->c_string = new char[this->size + 1];
+  strcpy(this->c_string, c_string);
+}
