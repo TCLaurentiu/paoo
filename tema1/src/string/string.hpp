0a4167755ea4ae3b635c7fa0d422f5d268c5fece
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-11-18 17:43:30 +0200

    Use the interface in the string header file

--- tema1/src/string/string.hpp
+++ tema1/src/string/string.hpp
@@ -1,21 +1,28 @@
 #pragma once
+#include "IString.hpp"
 
-class String{
+namespace MyString
+{
+
+  class String : public IString
+  {
   private:
     int size;
     int capacity;
     char *c_string;
     void resize(int new_size);
+
   public:
     String(const char *c_string);
     ~String();
-    String(const String& other);
-    String(String&& other);
+    String(const String &other);
+    String(String &&other);
 
     void setString(const char *c_string);
-    char *getString();
+    char *to_c_string() const;
 
+    void append(const char character);
     void append(const char *c_string);
-    void append(const String& other);
-
-};
+    void append(const IString &other);
+  };
+}
