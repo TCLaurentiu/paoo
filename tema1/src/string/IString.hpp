39fd49ec3b5e23688d1cf9676767fda7e3e1b576
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-11-18 16:55:59 +0200

    Make resize a private method in the interface, and fix append to be a pure virtual method

--- tema1/src/string/IString.hpp
+++ tema1/src/string/IString.hpp
@@ -1,11 +1,12 @@
-namespace String {
+namespace MyString {
   class IString{
     public:
-      virtual void resize(int new_size) = 0;
       virtual char* to_c_string() const = 0;
       virtual void setString(const char *string) = 0;
       virtual void append(const char character) = 0;
       virtual void append(const char *string) = 0;
-      virtual void append(const IString& string);
+      virtual void append(const IString& string) = 0;
+    private:
+      virtual void resize(int new_size) = 0;
   };
 }
