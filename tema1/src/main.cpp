fcda57ea28ba5493e827f0dcf1eff1711165ce51
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-11-19 18:19:16 +0200

    Test the String class in main

--- tema1/src/main.cpp
+++ tema1/src/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include "string.hpp"
+#include "morsecodestring.hpp"
 
 using std::cout;
 using MyString::String;
-
-String ret(String a){
-  return a;
-}
+using MyString::MorseCodeString;
 
 int main(){
-  String s_name("Claudiu");
-  String s_name2("Mircea");
+  String s1("Claudiu");
+  String s2(String("Andrei"));
+  String s3(s2);
+  s3 = String("Andreea");
+  s3 = s2;
+  s3.append("Iuliana");
+  s3.append(' ');
+  s3.append(s1);
 
-  s_name2 = String("Andrei");
-
-  s_name2.setString("Andreea");
+  cout << "\n";
+  cout << s1.to_c_string() << " " << s2.to_c_string() << " " << s3.to_c_string() << "\n";
+  cout << "\n";
 
 }
