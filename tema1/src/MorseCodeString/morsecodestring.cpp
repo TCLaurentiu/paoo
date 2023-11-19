a66f16f513bc660a54919343da2c5d764382b5fd
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-11-19 17:38:14 +0200

    Implement the MorseCodeCharacterToAlpha method which turns a string representing a morse code character into an alphanumerical character

--- tema1/src/MorseCodeString/morsecodestring.cpp
+++ tema1/src/MorseCodeString/morsecodestring.cpp
@@ -36,3 +36,32 @@ const char* MorseCodeString::morse_specials[3] = {
   "--..--",
   "..--..",
 };
+
+char MorseCodeString::MorseCodeCharacterToAlpha(const char* string){
+  int len = strlen(string);
+
+  if(len == SpecialLength){
+    for(size_t i = 0;i < SpecialCount;i++){
+      if(strcmp(string, MorseCodeString::morse_specials[i]) == 0){
+        return MorseCodeString::alpha_specials[i];
+      }
+    }
+  }
+
+  if(len == DigitLength){
+    for(size_t i = 0;i<DigitCount;i++){
+      if(strcmp(string, MorseCodeString::morse_digits[i]) == 0){
+        return '0' + i;
+      }
+    }
+  }
+
+  for(size_t i = 0;i<LetterCount;i++){
+    if(strcmp(string, MorseCodeString::morse_letters[i]) == 0){
+      return 'A' + i;
+    }
+  }
+
+  return -1;
+
+}
