833eeffa69c84fbffffdfcfd31da5789b60fa9d9
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-11-19 17:45:52 +0200

    Declare a no parameter constructor, the == operator and mark some methods as virtual

--- tema1/src/string/string.hpp
+++ tema1/src/string/string.hpp
@@ -13,18 +13,20 @@ namespace MyString
     void resize(int new_size);
 
   public:
+    String();
     String(const char *c_string);
     ~String();
     String(const String &other);
     String(String &&other);
     String& operator=(const String& other);
     String& operator=(String&& other);
+    bool operator==(const String& other) const;
 
-    void setString(const char *c_string);
+    virtual void setString(const char *c_string);
     char *to_c_string() const;
 
-    void append(const char character);
-    void append(const char *c_string);
-    void append(const IString &other);
+    virtual void append(const char character);
+    virtual void append(const char *c_string);
+    virtual void append(const IString &other);
   };
 }
