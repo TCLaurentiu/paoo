cb87d84e7d702d9f0ce85778af6d0b53a851de8c
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-11-19 17:34:30 +0200

    Add methods to convert from morse code to plain alphanumerical text

--- tema1/src/MorseCodeString/morsecodestring.hpp
+++ tema1/src/MorseCodeString/morsecodestring.hpp
@@ -23,15 +23,33 @@ namespace MyString
       void append(const char* c_string) override;
       void append(const char character) override;
       void append(const IString& other) override;
+      String ToAlphaNumerical();
 
     private:
 
+      /* 
+      checks if a string is valid morse code
+      that is, it is a space delimited string of . and -
+      where every group of . and - matches one of the
+      morse codes defined in morse_letters/digits/specials
+      returns the corresponding alpha-numerical String
+      or an empty string if the input is invalid
+      */
+      String MorseCodeToAlpha(const char *);
+
+      /*
+      converts a morse code character to its alpha numerical counterpart
+      returns -1 if the character is invalid
+      */
+      char MorseCodeCharacterToAlpha(const char *);
 
       static const char* morse_letters[LetterCount];
       static const char* morse_digits[DigitCount];
       
       static const char alpha_specials[SpecialCount];
       static const char* morse_specials[SpecialCount];
+
+      String AlphaNumerical;
 
   };
 }
