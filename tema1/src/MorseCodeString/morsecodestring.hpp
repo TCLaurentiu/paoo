81715c63511e65363df7df91e01a72f156a04202
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-11-19 17:33:38 +0200

    Add the character arrays that hold the morse codes for letters, digits and special characters

--- tema1/src/MorseCodeString/morsecodestring.hpp
+++ tema1/src/MorseCodeString/morsecodestring.hpp
@@ -1,6 +1,14 @@
 #pragma once
 #include "string.hpp"
 
+#define MaxLetterLength 4
+#define DigitLength 5
+#define SpecialLength 6
+#define SpecialCount 3
+#define DigitCount 10
+#define LetterCount 26
+#define MaxMorseCharacterLength 6
+
 namespace MyString
 {
   class MorseCodeString : public String
@@ -12,10 +20,18 @@ namespace MyString
 
       MorseCodeString& operator=(const MorseCodeString& other);
       MorseCodeString& operator=(MorseCodeString&& other);
-      void setString(const char* c_string) override;
       void append(const char* c_string) override;
       void append(const char character) override;
       void append(const IString& other) override;
+
+    private:
+
+
+      static const char* morse_letters[LetterCount];
+      static const char* morse_digits[DigitCount];
+      
+      static const char alpha_specials[SpecialCount];
+      static const char* morse_specials[SpecialCount];
 
   };
 }
