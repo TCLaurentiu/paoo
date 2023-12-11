e699a5d99746dec6aaf27cffe0f13ffbc2012096
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-12-11 11:09:54 +0200

    Make the members of the node public so they can be modified from linkedlist without extra work

--- tema3/src/LinkedList/linkedlist.hpp
+++ tema3/src/LinkedList/linkedlist.hpp
@@ -5,12 +5,10 @@
 #include <optional>
 
 typedef struct Node {
-  private:
-    std::pair<std::string, int> element;
-    std::shared_ptr<Node> next;
+  std::pair<std::string, int> element;
+  std::shared_ptr<Node> next;
 
   Node(std::pair<std::string, int>);
-
 } Node;
 
 typedef struct LinkedList {
