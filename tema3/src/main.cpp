25a087ecc079368007a12bbd1a47db6f493c9e30
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-12-11 14:47:00 +0200

    Define the global variables needed by the threads

--- tema3/src/main.cpp
+++ tema3/src/main.cpp
@@ -6,6 +6,12 @@
 #include "linkedlist.hpp"
 #include "stringinthashmap.hpp"
 
+const auto processor_count = std::thread::hardware_concurrency();
+
+StringIntHashMap map(1000);
+std::vector<std::string> words = read_file_as_word_vector("../res/mobydick.txt");
+int word_count = words.size();
+
 int main()
 {
 }
