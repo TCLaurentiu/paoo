7081488bb936fa6a08fb50147b72e94ad840173b
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-12-11 11:13:02 +0200

    Implement the print function

--- tema3/src/LinkedList/linkedlist.cpp
+++ tema3/src/LinkedList/linkedlist.cpp
@@ -1,4 +1,5 @@
 #include "linkedlist.hpp"
+#include <iostream>
 
 Node::Node(std::pair<std::string, int> element) {
   this->element = element;
@@ -13,4 +14,12 @@ void LinkedList::insert_front(std::pair<std::string, int> element) {
   std::shared_ptr<Node> new_node = std::make_shared<Node>(element);
   new_node -> next = this -> head;
   this -> head = new_node;
+}
+
+void LinkedList::print(){
+  std::shared_ptr<Node> iter = this->head;
+  while(iter != NULL){
+    std::cout << "String: " << iter->element.first << "Int:" << iter->element.second;
+    iter = iter->next;
+  }
 }
