12493885b50b5910c6a6ccd2d74da167eca3fcf7
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-12-11 11:10:32 +0200

    Implement the insert_front method

--- tema3/src/LinkedList/linkedlist.cpp
+++ tema3/src/LinkedList/linkedlist.cpp
@@ -8,3 +8,9 @@ Node::Node(std::pair<std::string, int> element) {
 LinkedList::LinkedList() {
   this->head = nullptr;
 }
+
+void LinkedList::insert_front(std::pair<std::string, int> element) {
+  std::shared_ptr<Node> new_node = std::make_shared<Node>(element);
+  new_node -> next = this -> head;
+  this -> head = new_node;
+}
