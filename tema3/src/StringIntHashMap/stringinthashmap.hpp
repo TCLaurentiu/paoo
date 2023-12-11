d12ad24ccce8282e17ecdbc0d6a5af939a3b32fd
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-12-11 10:34:36 +0200

    Create the StringIntHashMap folder and basic files

---
+++ tema3/src/StringIntHashMap/stringinthashmap.hpp
@@ -0,0 +1,9 @@
+/*
+a hashmap that has strings as keys and ints as values
+keys corresponding to a bucket are stored in a linked list
+insertion is done at the beginning of the list
+on access elements are moved to the beginning of the list
+*/
+class StringIntHashMap{
+
+};
