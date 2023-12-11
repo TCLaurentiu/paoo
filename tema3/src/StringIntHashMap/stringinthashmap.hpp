746fa39599562cc7c0154c6fdb199fa9c209f4d7
Claudiu Tirisi <claudiu.tirisi@student.upt.ro>
2023-12-11 10:39:14 +0200

    Declare typical hashmap methods:insert, exists, get and getHash

--- tema3/src/StringIntHashMap/stringinthashmap.hpp
+++ tema3/src/StringIntHashMap/stringinthashmap.hpp
@@ -1,3 +1,7 @@
+#pragma once
+#include <string>
+#include <vector>
+
 /*
 a hashmap that has strings as keys and ints as values
 keys corresponding to a bucket are stored in a linked list
@@ -5,5 +9,13 @@ insertion is done at the beginning of the list
 on access elements are moved to the beginning of the list
 */
 class StringIntHashMap{
+  public:
+    StringIntHashMap();
+    void insert(std::string key, int);
+    bool exists(std::string key);
+    int get(std::string key);
+
+  private:
+    int getHash(std::string);
 
 };
